1b4602dd25ef486cee8cd2bb7b12536eea12e85e
NAEEM UL HASSAN <2021cs667@student.uet.edu.pk>
2023-09-10 22:13:23 +0500

    Simple_interest

---
+++ simple_interest.sh
@@ -0,0 +1,44 @@
+# Project Code of Conduct
+
+## Introduction
+
+This project is dedicated to fostering an inclusive and welcoming community. We value the contributions of everyone who participates and want to create a positive and respectful environment for all. As such, we have established this code of conduct to outline our expectations for behavior within our project.
+
+## Expected Behavior
+
+We expect all participants to:
+
+- Be respectful and considerate of others' viewpoints, backgrounds, and experiences.
+- Use inclusive language and avoid offensive or harmful comments, jokes, or behavior.
+- Show empathy and kindness towards fellow contributors.
+- Listen actively and constructively to feedback and criticism.
+- Be patient and understanding with newcomers and those learning.
+
+## Unacceptable Behavior
+
+Unacceptable behavior includes but is not limited to:
+
+- Harassment, discrimination, or intimidation of any form.
+- Offensive or hurtful comments related to race, gender, sexual orientation, religion, disability, or any other protected characteristic.
+- Trolling, flaming, or other disruptive behavior.
+- Personal attacks, including public or private harassment.
+- Publishing or sharing private information without consent.
+
+## Enforcement
+
+If you observe or experience behavior that violates this code of conduct, please report it to [contact email or moderator/admin username]. All reports will be treated with confidentiality and will be reviewed and investigated promptly. 
+
+In cases where a violation is found, actions may be taken, including warnings, temporary or permanent bans from the project, or other appropriate measures to maintain a safe and welcoming environment.
+
+## Conclusion
+
+By participating in this project, you agree to abide by this code of conduct. We are committed to fostering a community where all individuals are treated with respect and dignity.
+
+This code of conduct is subject to change, so please check back periodically for updates.
+
+Thank you for helping us create a positive and inclusive environment for all contributors.
+
+---
+
+*This code of conduct is adapted from the Contributor Covenant, version 2.0, available at [https://www.contributor-covenant.org/version/2/0/code_of_conduct.html](https://www.contributor-covenant.org/version/2/0/code_of_conduct.html).*
+
